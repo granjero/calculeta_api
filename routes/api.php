39c827b3450d37b659d8585b794622abb5c76749
jm <juanmiguel.lombardisuzzi@gmail.com>
2023-12-03 18:40:46 -0300

    test

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
     $test = ["test" => "OK", "fecha" => date('Y-m-d H:i:s')];
-    return response()->json($test);
+    return response(date('Y-m-d H:i:s'));
 });
 Route::prefix('v1')->group(function () {
 
